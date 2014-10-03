ec1d408568ca3b32d18a28fe38a312e918dd7c18
Jason Lomnitz <jlomn@ucdavis.edu>
2014-10-03 14:41:34 -0700

    Updated the license information for many of the files that were missing the license preamble.

--- DSNVertexEnumeration.c
+++ DSNVertexEnumeration.c
@@ -1,10 +1,31 @@
-//
-//  DSNVertexEnumeration.c
-//  DesignSpaceToolboxV2
-//
-//  Created by Jason Lomnitz on 1/16/14.
-//
-//
+/**
+ * \file DSNVertexEnumeration.c
+ * \brief
+ *
+ * \details
+ *
+ * Copyright (C) 2011-2014 Jason Lomnitz.\n\n
+ *
+ * This file is part of the Design Space Toolbox V2 (C Library).
+ *
+ * The Design Space Toolbox V2 is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Design Space Toolbox V2 is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Design Space Toolbox. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * \author Jason Lomnitz.
+ * \date 2014
+ *
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
